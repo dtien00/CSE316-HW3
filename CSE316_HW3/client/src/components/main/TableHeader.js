e0a02de9e3841dd4fc2aa8851c4ee0c1d89a02d0
dtien00 <61069268+dtien00@users.noreply.github.com>
2021-04-01 10:56:30 -0400

    HW3_V1.3
    Added aesthetics for certain elements of the application:
    - Solid borders
    - Spacing/Alignment adjustment of columns/buttons

---
+++ CSE316_HW3/client/src/components/main/TableHeader.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { WButton, WRow, WCol } from 'wt-frontend';
+
+const TableHeader = (props) => {
+
+    const buttonStyle = props.disabled ? ' table-header-button-disabled ' : 'table-header-button ';
+    const clickDisabled = () => { };
+
+    return (
+        <WRow className="table-header">
+            <WCol size="4">
+                <WButton className='table-header-section' wType="texted" >Task</WButton>
+            </WCol>
+
+            <WCol size="2">
+                <WButton className='table-header-section' wType="texted">Due Date</WButton>
+            </WCol>
+
+            <WCol size="2">
+                <WButton className='table-header-section' wType="texted" >Status</WButton>
+            </WCol>
+
+            <WCol size="2">
+                <WButton className='table-header-section' wType="texted" >Assigned</WButton>
+            </WCol>
+
+
+            <WCol size="2">
+                <div className="table-header-buttons">
+                    
+                    <WButton className="sidebar-buttons undo-redo" onClick={props.undo} wType="texted" clickAnimation="ripple-light" shape="rounded">
+                        <i className="material-icons">undo</i>
+                    </WButton>
+                    
+                    <label className="col-spacer">&nbsp;</label>
+                    
+                    <WButton className="sidebar-buttons undo-redo" onClick={props.redo} wType="texted" clickAnimation="ripple-light" shape="rounded">
+                        <i className="material-icons">redo</i>
+                    </WButton>
+                    <WButton onClick={props.disabled ? clickDisabled : props.addItem} wType="texted" className={`${buttonStyle}`}>
+                        <i className="material-icons">add_box</i>
+                    </WButton>
+                    <WButton onClick={props.disabled ? clickDisabled : props.setShowDelete} wType="texted" className={`${buttonStyle}`}>
+                        <i className="material-icons">delete_outline</i>
+                    </WButton>
+                    <WButton onClick={props.disabled ? clickDisabled : () => props.setActiveList({})} wType="texted" className={`${buttonStyle}`}>
+                        <i className="material-icons">close</i>
+                    </WButton>
+                </div>
+            </WCol>
+
+        </WRow>
+    );
+};
+
+export default TableHeader;
